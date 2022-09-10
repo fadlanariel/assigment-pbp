dfada25f2e1ad58c6682de90a2aad4b735a27422
Fadlan Ariel Fathurrahman <fadlan.ariel@ui.ac.id>
2022-09-10 13:15:13 +0700

    implement views and models

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    'list_item': data_catalog_item,
+    'nama': 'Fadlan Ariel Fathurrahman',
+    'npm' : '2106750673',
+}
+
+def show_catalog(request):
+    return render(request, "katalog.html", context)
