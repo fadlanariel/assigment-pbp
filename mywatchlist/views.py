c53c5e6bdecbb61a485f8b041d44a5ed4cebdfc5
Fadlan Ariel Fathurrahman <fadlan.ariel@ui.ac.id>
2022-09-21 10:59:59 +0700

    add message implementation

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,10 +5,22 @@ from django.core import serializers
 
 # Create your views here.
 data_mywatchlist = MyWatchList.objects.all()
+mywatchlist_list = list(data_mywatchlist)
+watchcount = 0
+
+for film in mywatchlist_list:
+    if film.watched == True:
+        watchcount += 1
+
+if watchcount >= len(mywatchlist_list)/2:
+    msg = "Selamat, kamu sudah banyak menonton!"
+else:
+    msg = "Wah, kamu masih sedikit menonton!"
 context = {
     'watchlist': data_mywatchlist,
     'nama': 'Fadlan Ariel Fathurrahman',
     'npm' : '2106750673',
+    'message' : msg,
 }
 
 def show_watchlist(request):
