e67e522cdcbcc026b4b7fe40e1c85fb6fe0a20c0
Fadlan Ariel Fathurrahman <fadlan.ariel@ui.ac.id>
2022-09-21 19:54:40 +0700

    add message for number of watched film

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -5,11 +5,22 @@ from django.core import serializers
 
 # Create your views here.
 data_mywatchlist = MyWatchList.objects.all()
+data_list = list(data_mywatchlist)
+watchcount = 0
+for film in data_list:
+    if film.watched == True:
+        watchcount += 1
+
+if watchcount >= len(data_list)/2:
+    bonus_msg = "Selamat, kamu sudah banyak menonton!"
+else:
+    bonus_msg = "Wah, kamu masih sedikit menonton!"  
 
 context = {
     'watchlist': data_mywatchlist,
     'nama': 'Fadlan Ariel Fathurrahman',
     'npm' : '2106750673',
+    'message' : bonus_msg,
 
 }
 
