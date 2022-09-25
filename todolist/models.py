495ad50d7ac26c36dc0535feea9bbfa5fc4d0365
Fadlan Ariel Fathurrahman <fadlan.ariel@ui.ac.id>
2022-09-25 19:53:56 +0700

    start todolist app

---
+++ todolist/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class MyTodoList(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateField()
+    title = models.CharField(max_length=100)
+    description = models.TextField()
